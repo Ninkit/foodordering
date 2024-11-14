f0ed84a2cc234582cbcf3d1cbd366c0bc2d07d11
Ninkit <truesome92@gmail.com>
2024-11-14 02:13:22 -0800

    fixed error

--- src/app/api/register/route.tsx
+++ src/app/api/register/route.tsx
@@ -1,17 +1,17 @@
-import { User } from "@/app/models/User";
-import mongoose from "mongoose"
+// import { User } from "@/app/models/User";
+// import mongoose from "mongoose"
 
-export async function POST(req: { json: () => unknown }) {
-    const body = await req.json();
+// export async function POST(req: { json: () => unknown }) {
+//     const body = await req.json();
     
-    const mongoUrl = process.env.MONGO_URL;
-    if (!mongoUrl) {
-    throw new Error("MONGO_URL environment variable is not defined");
-    }
-    await mongoose.connect(mongoUrl);
+//     const mongoUrl = process.env.MONGO_URL;
+//     if (!mongoUrl) {
+//     throw new Error("MONGO_URL environment variable is not defined");
+//     }
+//     await mongoose.connect(mongoUrl);
 
-    const createUser = await User.create(body)
-    return Response.json(createUser);
+//     const createUser = await User.create(body)
+//     return Response.json(createUser);
     
 
-}
+// }
